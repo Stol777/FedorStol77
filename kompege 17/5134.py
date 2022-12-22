a2e977797f768ee2bd711147489e02f1e5e901be
mac <deutsch2288@yandex.ru>
2022-12-22 14:09:55 +0300

    1

---
+++ kompege 17/5134.py
@@ -0,0 +1,14 @@
+with open('17_5134.txt') as f:
+    seq = list(map(int, f.readlines()))
+    lst = []
+    for item in seq:
+        if str(item)[-2::] == '10':
+            lst.append(item)
+    max_element = max(lst)
+    lst = []
+    counter = 0
+    for i in range(len(seq) - 1):
+        if (seq[i] % 2023) * (seq[i + 1] % 2023) >= max_element:
+            counter += 1
+            lst.append(seq[i] + seq[i + 1])
+    print(counter, min(lst))
