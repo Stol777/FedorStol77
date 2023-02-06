725db253a96e8e61963212f995cb76a9f4dbce29
mac <deutsch2288@yandex.ru>
2023-02-07 00:49:38 +0300

    1

---
+++ email-sender/main_2.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_email(message):
+    sender = "sender@yandex.ru"
+    password = 'qwerty123'
+
+    server = smtplib.SMTP("smtp.yandex.ru", 465)
+    server.starttls()
+
+    try:
+        server.login(sender, password)
+        msg = MIMEText(message)
+        msg["Subject"] = "Тема сообщения"
+        server.sendmail(sender, sender, msg.as_string())
+
+        return "Сообщение отправлено!"
+    except Exception as _ex:
+        return f"{_ex}\nПроверьте логин или пароль!"
