0ec0647d5b9f35c333928cfbbb3896bbcd6e66b8
mac <deutsch2288@yandex.ru>
2023-02-07 18:32:55 +0300

    1

--- email-sender/main_2.py
+++ email-sender/main_2.py
@@ -2,19 +2,33 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-def send_email(message):
-    sender = "sender@yandex.ru"
-    password = 'qwerty123'
+def send_email(message, subject):
+    sender = "Bujvolisimus@yandex.ru"
+    password = 'ijzzcephfwbhgyxz'
+    # sender = 'ivanivanov777moscow@mail.ru'
+    # password = 'yep0PXE5datKhtMrN6aN'
+    # sender = 'pythonproject@rambler.ru'
+    # password = '68RfYFHE3xYE@vJ'
 
-    server = smtplib.SMTP("smtp.yandex.ru", 465)
-    server.starttls()
+    server = smtplib.SMTP_SSL("smtp.yandex.ru", 465)
+    # server.starttls()
 
     try:
         server.login(sender, password)
         msg = MIMEText(message)
-        msg["Subject"] = "Тема сообщения"
-        server.sendmail(sender, sender, msg.as_string())
+        msg["Subject"] = subject
+        server.sendmail(sender, 'deutsch2288@yandex.ru', msg.as_string())
         return "Сообщение отправлено!"
 
     except Exception as _ex:
-        return f"{_ex}\nПроверьте логин или пароль!"
+        return f"{_ex}\nВозможная проблема: проверьте логин или пароль"
+
+
+def main():
+    message = input('Введите сообщение: ')
+    subject = input('Введите тему сообщения: ')
+    print(send_email(message, subject))
+
+
+if __name__ == '__main__':
+    main()
